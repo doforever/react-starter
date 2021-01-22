ecebdae69c5012941e090313aafa6531bba6407b
Dorota Tomala <doforever@gmail.com>
2021-01-22 11:25:13 +0100

    Rewrite card as functional component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,18 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Card.scss';
 
-class Card extends React.Component {
-  static propTypes = {
-    title: PropTypes.node.isRequired,
-  }
+const Card = props => (
+  <div className={styles.component}>
+    <h4>{props.title}</h4>
+  </div>
+);
 
-  render() {
-    return (
-      <div className={styles.component}>
-        <h4>{this.props.title}</h4>
-      </div>
-    );
-  }
-}
+Card.propTypes = {
+  title: PropTypes.node.isRequired,
+};
 
 export default Card;
