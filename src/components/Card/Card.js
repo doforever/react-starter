f8aef8f320cebb2e9f74f0eea921722437048aec
Dorota Tomala <doforever@gmail.com>
2021-01-27 19:21:56 +0100

    Render components with store

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,11 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Card.scss';
 
-const Card = props => (
-  <div className={styles.component}>
-    <h4>{props.title}</h4>
-  </div>
-);
+const Card = props => {
+  const {title} = props;
+
+  return (
+    <div className={styles.component}>
+      <h4>{title}</h4>
+    </div>
+  );
+};
 
 Card.propTypes = {
   title: PropTypes.node.isRequired,
